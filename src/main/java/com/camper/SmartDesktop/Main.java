861625a0868cae6a82e928d4e4c88bd6421f50a5
CamperGit <77017039+CamperGit@users.noreply.github.com>
2021-02-01 23:50:53 +0300

    Refactor Resources folder and add size of fullscreen user window

--- src/main/java/com/camper/SmartDesktop/Main.java
+++ src/main/java/com/camper/SmartDesktop/Main.java
@@ -9,23 +9,22 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application
 {
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
+    public static void main(String[] args) { launch(args); }
 
     @Override
     public void start(Stage stage) throws IOException
     {
         var loader = Main.class.getClassLoader();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(loader.getResource("com/camper/SmartDesktop/StartScreen.fxml")));
-        System.out.println(System.getProperty("user.dir"));
-        var scene = new Scene(root,300,275);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(loader.getResource("FXMLs/StartScreen.fxml")));
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        var scene = new Scene(root,screenSize.width,screenSize.height-66);
         stage.setScene(scene);
         stage.setTitle("SmartDesktop");
         stage.show();
