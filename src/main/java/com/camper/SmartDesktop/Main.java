1b6032d753ffbd723a54cbf1ba7229354a2336a3
CamperGit <77017039+CamperGit@users.noreply.github.com>
2021-02-02 17:10:49 +0300

    Refactor Resources folder and add size of fullscreen user window

--- src/main/java/com/camper/SmartDesktop/Main.java
+++ src/main/java/com/camper/SmartDesktop/Main.java
@@ -3,30 +3,64 @@ package com.camper.SmartDesktop;
 
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Main extends Application
+public class Main extends Application implements Initializable
 {
     public static void main(String[] args) { launch(args); }
+
+    @FXML private MediaView videoViewer;
+    private static final int DEFAULT_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
+    private static final int DEFAULT_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
+    private ExecutorService executor = Executors.newCachedThreadPool();
 
     @Override
     public void start(Stage stage) throws IOException
     {
         var loader = Main.class.getClassLoader();
         Parent root = FXMLLoader.load(Objects.requireNonNull(loader.getResource("FXMLs/StartScreen.fxml")));
-        var kit = Toolkit.getDefaultToolkit();
-        var screenSize = kit.getScreenSize();
-        var scene = new Scene(root,screenSize.width,screenSize.height-66);
+        var scene = new Scene(root,DEFAULT_WIDTH,DEFAULT_HEIGHT-66);
         stage.setScene(scene);
         stage.setTitle("SmartDesktop");
         stage.show();
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        String VUrl = "file:/E:/Programming/IdeaProjects/SmartDesktop/src/main/resources/test.mp4";
+        var media = new Media(VUrl);
+        var mediaPlayer = new MediaPlayer(media);
+        videoViewer.setFitHeight(DEFAULT_HEIGHT);
+        videoViewer.setFitWidth(DEFAULT_WIDTH);
+        videoViewer.setMediaPlayer(mediaPlayer);
+        mediaPlayer.play();
+        mediaPlayer.setOnEndOfMedia(()->
+                {
+                    mediaPlayer.stop();
+                    videoViewer.setMediaPlayer(mediaPlayer);
+                    mediaPlayer.play();
+                });
+
     }
 }
