236c86cbfe2d2cddfea955136a41299a22eed0b6
CamperGit <sashchenkodmitrii@mail.ru>
2021-02-17 10:44:24 +0300

    add shadows

--- src/main/java/com/camper/SmartDesktop/Info/ScheduleSD.java
+++ src/main/java/com/camper/SmartDesktop/Info/ScheduleSD.java
@@ -14,6 +14,7 @@ import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
